db9f6d00daee9e95ca64d3ea15275acfbb8ae92c
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-06-04 22:43:15 +0200

    feat: add keymaps to copy and paste buffer
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<C-c>", function()
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local content = table.concat(lines, "\n")
+  vim.fn.setreg("+", content)
+  vim.notify("Buffer copied to clipboard", vim.log.levels.INFO)
+end, { desc = "Copy buffer to clipboard" })
+
+vim.keymap.set("n", "<C-v>", function()
+  local content = vim.fn.getreg("+")
+  local lines = vim.split(content, "\n")
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+  vim.notify("Buffer replaced from clipboard", vim.log.levels.INFO)
+end, { desc = "Paste from clipboard (replace buffer)" })
